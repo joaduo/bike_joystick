d63aecc54e1a0bdb3dc699e333746d7cde43db33
jduo <jduo@juju.com>
2018-04-12 13:51:17 -0300

    Add --top argument

--- bike_joystick.py
+++ bike_joystick.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2018  Joaquin Duo under GPL v3
 from evdev import UInput, ecodes
 from arduino_listener import yield_bike_msgs, prnt
+import argparse
 
 class VirtualJoystick(object):
     def __init__(self, max=300):
@@ -17,10 +18,15 @@ class VirtualJoystick(object):
         self.joystick.syn()
 
 
-def run_virtual_bike(max=300):
+def run_virtual_bike(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--top', type=int, help='top rpm we think we '
+                        'can make (means 100%% joystick)')
+    args = parser.parse_args(args)
     vbike = VirtualJoystick()
+    top_rpm = args.top
     for rpm, ratio in yield_bike_msgs(arduino_dev='/dev/ttyACM0'):
-        value = int(rpm*255./300.)
+        value = int(rpm*255./top_rpm)
         prnt('rpm=%.3f ratio=%.3f', rpm, ratio)
         prnt('value=%s', value)
         vbike.signal(value)
