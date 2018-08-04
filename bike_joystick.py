ed21c17dfe5582f15e40abaa964a173b4a4278f7
jduo <jduo@juju.com>
2018-08-04 05:17:00 -0300

    Use new evdev library. Fix top rpm

--- bike_joystick.py
+++ bike_joystick.py
@@ -2,12 +2,38 @@
 from evdev import UInput, ecodes
 from arduino_listener import yield_bike_msgs, prnt
 import argparse
+from evdev.device import AbsInfo
 
 class VirtualJoystick(object):
-    def __init__(self, max=300):
-        #events = {ecodes.EV_ABS: [(ecodes.ABS_X, (0, 255, 5, 0))],}
-        events = {ecodes.EV_ABS: [(ecodes.ABS_X, (-255, 255, 5, 0))],}
-        self.joystick = UInput(name='virtual_bike', events=events)
+    def __init__(self, top_rpm):
+        cap = {1L: set([288L,
+                  289L,
+                  290L,
+                  291L,
+                  292L,
+                  293L,
+                  294L,
+                  295L,
+                  296L,
+                  297L,
+                  298L,
+                  299L]),
+         3L: set([(0,
+                   AbsInfo(value=top_rpm, min=0, max=top_rpm*2, fuzz=0, flat=15, resolution=0)),
+                  (1L,
+                   AbsInfo(value=128, min=0, max=255, fuzz=0, flat=15, resolution=0)),
+                  (2L,
+                   AbsInfo(value=134, min=0, max=255, fuzz=0, flat=15, resolution=0)),
+                  (3L,
+                   AbsInfo(value=128, min=0, max=255, fuzz=0, flat=15, resolution=0)),
+                  (4L,
+                   AbsInfo(value=128, min=0, max=255, fuzz=0, flat=15, resolution=0)),
+                  (16L,
+                   AbsInfo(value=0, min=-1, max=1, fuzz=0, flat=0, resolution=0)),
+                  (17L,
+                   AbsInfo(value=0, min=-1, max=1, fuzz=0, flat=0, resolution=0))]),
+         4L: set([4L])}
+        self.joystick = UInput(cap, name='gpad-bike')
 
     def __del__(self):
         self.joystick.close()
@@ -20,15 +46,20 @@ class VirtualJoystick(object):
 
 def run_virtual_bike(args=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--top', type=int, help='top rpm we think we '
+    parser.add_argument('-t', '--top', type=int, default=714,
+                        help='top rpm we think we '
                         'can make (means 100%% joystick)')
     args = parser.parse_args(args)
-    vbike = VirtualJoystick()
-    top_rpm = args.top
+    vbike = VirtualJoystick(args.top)
+    max_measure = 0
     for rpm, ratio in yield_bike_msgs(arduino_dev='/dev/ttyACM0'):
-        value = int(rpm*255./top_rpm)
+        if not rpm:
+            max_measure = 0
+        max_measure = max(rpm, max_measure)
+        value = int(rpm + args.top)
         prnt('rpm=%.3f ratio=%.3f', rpm, ratio)
         prnt('value=%s', value)
+        prnt('top=%s', max_measure)
         vbike.signal(value)
 
 
